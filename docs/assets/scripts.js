da1d6535ce268574336082c1adb4616d2244eb86
Kayoo <jeri.dsql@gmail.com>
2024-11-15 16:34:30 +0100

    change menu

--- docs/assets/scripts.js
+++ docs/assets/scripts.js
@@ -1,12 +1,10 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-} 
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
 
 /* Image loading */
 window.onload = function() {
