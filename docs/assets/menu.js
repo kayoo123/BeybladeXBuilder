0731b72b089995bfb61fdd5fdd95551c1862c90d
Kayoo <jeri.dsql@gmail.com>
2024-11-15 17:26:20 +0100

    test

---
+++ docs/assets/menu.js
@@ -0,0 +1,22 @@
+document.write('\
+\
+<div id="mySidepanel" class="sidepanel">\
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>\
+    <a href="/BeybladeXBuilder/news.html">News</a>\
+    <a href="#">Services</a>\
+    <a href="#">Clients</a>\
+    <a href="#">Contact</a>\
+  </div>\
+  \
+  <table style="width: 100%;">\
+      <tr>\
+          <td style="text-align: left;">\
+              <a href="/BeybladeXBuilder/" title="Index"><img src="https://beyblade.com/assets/images/company-logo-3.webp" alt="company-logo-3"></img></a>\
+          </td>\
+          <td style="text-align: right;">\
+              <button class="openbtn" onclick="openNav()">☰</button>\
+          </td>\
+      </tr>\
+  </table>\
+\
+');
